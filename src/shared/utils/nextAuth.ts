c11791cef0b2bb065ae55b00b37890fb5a53eb8b
hihhhello <grigori.ratushniak@gmail.com>
2023-11-13 20:02:41 -0600

    avoid fake ssl

--- src/shared/utils/nextAuth.ts
+++ src/shared/utils/nextAuth.ts
@@ -16,33 +16,42 @@ export const NEXT_AUTH_OPTIONS: AuthOptions = {
         },
       },
       async authorize(credentials, req) {
-        const res = await fetch(`${process.env.SERVER_URL}/signin`, {
-          method: 'POST',
-          body: JSON.stringify({
-            password: credentials?.password,
-            email: credentials?.username,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-
-        const user: { access_token: string } = await res.json();
-
-        if (res.ok && user) {
-          const userInfoResponse = await fetch(`${process.env.SERVER_URL}/me`, {
-            method: 'GET',
+        try {
+          const res = await fetch(`${process.env.SERVER_URL}/signin`, {
+            method: 'POST',
+            body: JSON.stringify({
+              password: credentials?.password,
+              email: credentials?.username,
+            }),
             headers: {
               'Content-Type': 'application/json',
-              Authorization: `Bearer ${user.access_token}`,
             },
           });
 
-          const userInfo: { email: string; id: string } =
-            await userInfoResponse.json();
+          const user: { access_token: string } = await res.json();
 
-          return { ...user, ...userInfo };
-        } else {
+          if (res.ok && user) {
+            const userInfoResponse = await fetch(
+              `${process.env.SERVER_URL}/me`,
+              {
+                method: 'GET',
+                headers: {
+                  'Content-Type': 'application/json',
+                  Authorization: `Bearer ${user.access_token}`,
+                },
+              },
+            );
+
+            const userInfo: { email: string; id: string } =
+              await userInfoResponse.json();
+
+            return { ...user, ...userInfo };
+          } else {
+            return null;
+          }
+        } catch (error) {
+          console.log(error);
+
           return null;
         }
       },
