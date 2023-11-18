79418be80d050ff50f5f81b0a15d22582a516161
hihhhello <grigori.ratushniak@gmail.com>
2023-11-18 09:22:01 -0600

    add navbar and sign out button

--- src/app/_ui/BaseLayout.tsx
+++ src/app/_ui/BaseLayout.tsx
@@ -1,87 +1,20 @@
-'use client';
-
 import { ReactNode } from 'react';
-import { usePathname } from 'next/navigation';
-import { BellIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-
-import { classNames } from '@/shared/utils/helpers';
-import { CloudIcon } from '@/shared/ui/Icons/CloudIcon';
+import { Navbar } from './Navbar';
 
 type BaseLayoutProps = {
   children: ReactNode;
 };
 
-const NAVIGATION_ITEMS = [
-  {
-    title: 'Dashboard',
-    href: '/',
-  },
-  {
-    title: 'Spending Plan',
-    href: '/spending-plan',
-  },
-  {
-    title: 'Saving Goals',
-    href: '/saving-goals',
-  },
-  {
-    title: 'Linked Accounts',
-    href: '/linked-accounts',
-  },
-  {
-    title: 'Settings',
-    href: '/settings',
-  },
-];
-
-const Header = ({ pathname }: { pathname: string }) => {
+export const BaseLayout = async ({ children }: BaseLayoutProps) => {
   return (
-    <div className="flex justify-between">
-      <div className="flex gap-1">
-        <div className="flex-center h-14 w-14 rounded-full bg-main-blue">
-          <CloudIcon />
-        </div>
+    <div>
+      <Navbar />
 
-        <div className="flex rounded-[100px] bg-main-white">
-          {NAVIGATION_ITEMS.map(({ title, href }) => (
-            <div
-              key={title}
-              className={classNames(
-                'flex-center rounded-[100px] px-6',
-                href === pathname && 'bg-main-dark',
-              )}
-            >
-              <Link href={href}>
-                <span
-                  className={classNames(
-                    'whitespace-nowrap text-sm text-main-dark ',
-                    href === pathname && 'text-white',
-                  )}
-                >
-                  {title}
-                </span>
-              </Link>
-            </div>
-          ))}
-        </div>
+      <div className="px-4 sm:px-[120px]">
+        <main className="container mx-auto">
+          <div className="pt-2 sm:pt-10">{children}</div>
+        </main>
       </div>
-
-      <div className="flex items-center gap-8 rounded-[100px] bg-main-white py-1 pl-6 pr-2">
-        <BellIcon className="h-6 w-6" />
-
-        <div className="h-12 w-12 rounded-full bg-white"></div>
-      </div>
-    </div>
-  );
-};
-
-export const BaseLayout = ({ children }: BaseLayoutProps) => {
-  return (
-    <div className="px-4 sm:px-[120px]">
-      <main className="container mx-auto">
-        <div className="pt-2 sm:pt-10">{children}</div>
-      </main>
     </div>
   );
 };
---
+++ src/app/_ui/Navbar.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { signOut } from 'next-auth/react';
+
+export const Navbar = () => {
+  return (
+    <div className="h-16 w-full px-4 shadow-md">
+      <div className="flex h-full w-full items-center justify-end">
+        <button
+          onClick={() => signOut()}
+          className="rounded bg-indigo-600 px-3 py-1.5 leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Sign Out
+        </button>
+      </div>
+    </div>
+  );
+};
