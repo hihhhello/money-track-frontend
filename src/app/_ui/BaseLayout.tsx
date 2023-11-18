0c27fc01417b9a0447a4d25f4f7177c9a49e9d78
hihhhello <grigori.ratushniak@gmail.com>
2023-11-18 10:35:16 -0600

    remove navbar for non auth users

--- src/app/_ui/BaseLayout.tsx
+++ src/app/_ui/BaseLayout.tsx
@@ -1,14 +1,18 @@
 import { ReactNode } from 'react';
 import { Navbar } from './Navbar';
+import { getServerSession } from 'next-auth';
+import { NEXT_AUTH_OPTIONS } from '@/shared/utils/nextAuth';
 
 type BaseLayoutProps = {
   children: ReactNode;
 };
 
 export const BaseLayout = async ({ children }: BaseLayoutProps) => {
+  const session = await getServerSession(NEXT_AUTH_OPTIONS);
+
   return (
     <div>
-      <Navbar />
+      {session?.user && <Navbar />}
 
       <div className="px-4 sm:px-[120px]">
         <main className="container mx-auto">
