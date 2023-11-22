8ccc97adc23408fb3ad242af8f150aee447a0509
hihhhello <grigori.ratushniak@gmail.com>
2023-11-21 18:18:34 -0600

    add creating new category modal and logic

--- src/app/_ui/SettingsSidebar/ui/CategoriesDisclosure.tsx
+++ src/app/_ui/SettingsSidebar/ui/CategoriesDisclosure.tsx
@@ -1,12 +1,27 @@
+import { Fragment, useMemo, useState } from 'react';
+
+import { ManageCategoryModal } from '@/shared/ManageCategoryModal';
 import { api } from '@/shared/api/api';
 import { PlusIcon } from '@/shared/ui/Icons/PlusIcon';
 import { SquaresPlusIcon } from '@/shared/ui/Icons/SquaresPlusIcon';
+import { useBoolean, useLoadingToast } from '@/shared/utils/hooks';
 import { Disclosure, Transition } from '@headlessui/react';
 import { useQuery } from '@tanstack/react-query';
-import { Fragment, useMemo } from 'react';
 
 export const CategoriesDisclosure = () => {
-  const { data: categories } = useQuery({
+  const loadingToast = useLoadingToast();
+
+  const {
+    value: isAddNewCategoryModalOpen,
+    setTrue: handleOpenAddNewCategoryModal,
+    setFalse: handleCloseAddNewCategoryModal,
+  } = useBoolean(false);
+
+  const [categoryTypeToAdd, setCategoryTypeToaAdd] = useState<
+    null | 'deposit' | 'expense'
+  >();
+
+  const { data: categories, refetch: refetchCategories } = useQuery({
     queryFn: api.categories.getAll,
     queryKey: ['api.categories.getAll'],
   });
@@ -44,69 +59,120 @@ export const CategoriesDisclosure = () => {
     [categories],
   );
 
+  const handleAddNewExpenseCategory = () => {
+    setCategoryTypeToaAdd('expense');
+    handleOpenAddNewCategoryModal();
+  };
+
+  const handleAddNewDepositCategory = () => {
+    setCategoryTypeToaAdd('deposit');
+    handleOpenAddNewCategoryModal();
+  };
+
+  const handleAddNewCategory = (categoryName: string) => {
+    if (!categoryTypeToAdd) {
+      return;
+    }
+
+    const toastId = loadingToast.showLoading('Adding your new category...');
+
+    return api.categories
+      .createOne({
+        body: {
+          name: categoryName,
+          type: categoryTypeToAdd,
+        },
+      })
+      .then(() => {
+        refetchCategories();
+
+        loadingToast.handleSuccess({
+          toastId,
+          message: 'You successfully added a new category!',
+        });
+      })
+      .catch(() => {
+        loadingToast.handleError({
+          toastId,
+          message:
+            'Something gone wrong while adding your category. Try again.',
+        });
+      });
+  };
+
   return (
-    <Disclosure>
-      <Disclosure.Button className="flex w-full flex-col items-center py-4">
-        <SquaresPlusIcon className="h-12 w-12 text-indigo-600" />
+    <>
+      <Disclosure>
+        <Disclosure.Button className="flex w-full flex-col items-center py-4">
+          <SquaresPlusIcon className="h-12 w-12 text-indigo-600" />
 
-        <span>Categories</span>
-      </Disclosure.Button>
+          <span>Categories</span>
+        </Disclosure.Button>
 
-      <Transition
-        enter="transition duration-100 ease-out"
-        enterFrom="transform scale-95 opacity-0"
-        enterTo="transform scale-100 opacity-100"
-        leave="transition duration-75 ease-out"
-        leaveFrom="transform scale-100 opacity-100"
-        leaveTo="transform scale-95 opacity-0"
-        as={Fragment}
-      >
-        <Disclosure.Panel className="max-h-44 min-h-full w-full overflow-y-auto">
-          <div className="bg-indigo-500">
-            <div>
-              <div className="flex w-full items-center justify-between bg-indigo-400 px-4 py-2">
-                <span className="text-white">Expense</span>
+        <Transition
+          enter="transition duration-100 ease-out"
+          enterFrom="transform scale-95 opacity-0"
+          enterTo="transform scale-100 opacity-100"
+          leave="transition duration-75 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+          as={Fragment}
+        >
+          <Disclosure.Panel className="max-h-44 min-h-full w-full overflow-y-auto">
+            <div className="bg-indigo-500">
+              <div>
+                <div className="flex w-full items-center justify-between bg-indigo-400 px-4 py-2">
+                  <span className="text-white">Expense</span>
 
-                <button>
-                  <PlusIcon className="text-white" />
-                </button>
+                  <button onClick={handleAddNewExpenseCategory}>
+                    <PlusIcon className="text-white" />
+                  </button>
+                </div>
+
+                <div className="flex flex-col">
+                  {reducedCategories?.expense.map((category) => (
+                    <button
+                      className="px-4 py-2 text-left text-white hover:bg-indigo-400"
+                      key={category.id}
+                    >
+                      {category.name}
+                    </button>
+                  ))}
+                </div>
               </div>
 
-              <div className="flex flex-col">
-                {reducedCategories?.expense.map((category) => (
-                  <button
-                    className="px-4 py-2 text-left text-white hover:bg-indigo-400"
-                    key={category.id}
-                  >
-                    {category.name}
+              <div>
+                <div className="flex w-full items-center justify-between bg-indigo-400 px-4 py-2">
+                  <span className="text-white">Deposit</span>
+
+                  <button onClick={handleAddNewDepositCategory}>
+                    <PlusIcon className="text-white" />
                   </button>
-                ))}
+                </div>
+
+                <div className="flex flex-col">
+                  {reducedCategories?.deposit.map((category) => (
+                    <button
+                      className="px-4 py-2 text-left text-white hover:bg-indigo-400"
+                      key={category.id}
+                    >
+                      {category.name}
+                    </button>
+                  ))}
+                </div>
               </div>
             </div>
+          </Disclosure.Panel>
+        </Transition>
+      </Disclosure>
 
-            <div>
-              <div className="flex w-full items-center justify-between bg-indigo-400 px-4 py-2">
-                <span className="text-white">Deposit</span>
-
-                <button>
-                  <PlusIcon className="text-white" />
-                </button>
-              </div>
-
-              <div className="flex flex-col">
-                {reducedCategories?.deposit.map((category) => (
-                  <button
-                    className="px-4 py-2 text-left text-white hover:bg-indigo-400"
-                    key={category.id}
-                  >
-                    {category.name}
-                  </button>
-                ))}
-              </div>
-            </div>
-          </div>
-        </Disclosure.Panel>
-      </Transition>
-    </Disclosure>
+      <ManageCategoryModal
+        handleClose={handleCloseAddNewCategoryModal}
+        handleSubmit={handleAddNewCategory}
+        isModalOpen={isAddNewCategoryModalOpen}
+        title="Add new category"
+        submitButtonLabel="Add"
+      />
+    </>
   );
 };
---
+++ src/shared/ManageCategoryModal.tsx
@@ -0,0 +1,107 @@
+'use client';
+
+import { FormEvent, Fragment, useState } from 'react';
+import { toast } from 'react-toastify';
+import { Dialog, Transition } from '@headlessui/react';
+import { XMarkIcon } from '@/shared/ui/Icons/XMarkIcon';
+import { isEmpty } from 'lodash';
+
+type ManageCategoryModalProps = {
+  isModalOpen: boolean;
+  handleClose: () => void;
+  handleSubmit: (categoryName: string) => Promise<void> | undefined;
+  submitButtonLabel?: string;
+  title: string;
+  defaultCategoryName?: string;
+};
+
+export const ManageCategoryModal = ({
+  handleClose,
+  isModalOpen,
+  handleSubmit: propsHandleSubmit,
+  title,
+  submitButtonLabel,
+  defaultCategoryName,
+}: ManageCategoryModalProps) => {
+  const [categoryName, setCategoryName] = useState(defaultCategoryName ?? '');
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (!categoryName || isEmpty(categoryName.trim())) {
+      return toast.warn(
+        'The category name should contain at least 1 character.',
+      );
+    }
+
+    propsHandleSubmit(categoryName)?.then(() => setCategoryName(''));
+  };
+
+  return (
+    <Transition show={isModalOpen} as={Fragment}>
+      <Dialog onClose={handleClose} as="div" className="relative z-50">
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 hidden bg-black/30 sm:block" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 flex w-screen items-center justify-center sm:p-4">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Dialog.Panel className="relative h-full w-full bg-white p-4 pb-20 sm:h-auto sm:max-w-sm sm:rounded">
+              <div className="flex justify-end">
+                <button onClick={handleClose}>
+                  <XMarkIcon />
+                </button>
+              </div>
+
+              <Dialog.Title
+                as="h3"
+                className="text-base font-semibold leading-6 text-gray-900"
+              >
+                {title}
+              </Dialog.Title>
+
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4 flex flex-col">
+                  <label htmlFor="categoryName">Name</label>
+                  <input
+                    className="focus:ring-primary-green block w-full rounded-md border-0 px-4 py-1.5 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-base"
+                    name="categoryName"
+                    value={categoryName}
+                    onChange={(e) => {
+                      setCategoryName(e.target.value);
+                    }}
+                  />
+                </div>
+
+                <div className="absolute bottom-0 left-0 w-full p-4">
+                  <button
+                    type="submit"
+                    className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    {submitButtonLabel ?? 'Submit'}
+                  </button>
+                </div>
+              </form>
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
