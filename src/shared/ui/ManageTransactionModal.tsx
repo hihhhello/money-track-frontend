cfbb58aa46895cf52b1a7113e4d1ed1696327a13
hihhhello <grigori.ratushniak@gmail.com>
2023-11-24 15:00:39 -0600

    add modal scrolling content

--- src/shared/ui/ManageTransactionModal.tsx
+++ src/shared/ui/ManageTransactionModal.tsx
@@ -63,9 +63,7 @@ export const ManageTransactionModal = ({
       description: defaultValues?.description ?? null,
     });
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-
+  const handleSubmit = () => {
     if (!selectedCategoryId) {
       return toast.warn('Select category.');
     }
@@ -121,107 +119,113 @@ export const ManageTransactionModal = ({
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <Dialog.Panel className="relative h-full w-full overflow-y-auto bg-white p-4 sm:max-w-5xl sm:rounded">
-              <div
-                className={classNames(
-                  'mb-8 flex',
-                  handleDelete ? 'justify-between' : 'justify-end',
-                )}
-              >
-                {handleDelete && (
-                  <button onClick={handleDeleteTransaction}>
-                    <TrashIcon className="text-red-600 hover:text-red-500" />
-                  </button>
-                )}
+            <Dialog.Panel className="relative flex h-full w-full flex-col bg-white sm:max-h-[550px] sm:max-w-5xl sm:rounded">
+              <div className="z-10 p-4 shadow-md">
+                <div
+                  className={classNames(
+                    'flex',
+                    handleDelete ? 'justify-between' : 'justify-end',
+                  )}
+                >
+                  {handleDelete && (
+                    <button onClick={handleDeleteTransaction}>
+                      <TrashIcon className="text-red-600 hover:text-red-500" />
+                    </button>
+                  )}
 
-                <button onClick={handleClose}>
-                  <XMarkIcon />
-                </button>
+                  <button onClick={handleClose}>
+                    <XMarkIcon />
+                  </button>
+                </div>
+
+                <Dialog.Title
+                  as="h3"
+                  className="text-base font-semibold leading-6 text-gray-900"
+                >
+                  {title}
+                </Dialog.Title>
               </div>
 
-              <Dialog.Title
-                as="h3"
-                className="text-base font-semibold leading-6 text-gray-900"
-              >
-                {title}
-              </Dialog.Title>
+              <div className="h-full overflow-y-auto p-4">
+                <div>
+                  <div className="mb-4 flex flex-col">
+                    <label htmlFor="amount">Amount</label>
+                    <input
+                      className="focus:ring-primary-green block w-full rounded-md border-0 px-4 py-1.5 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-base"
+                      type="number"
+                      name="amount"
+                      value={String(transactionFormValues.amount)}
+                      onChange={(e) => {
+                        setTransactionFormValues((prevValues) => ({
+                          ...prevValues,
+                          amount: e.target.value,
+                        }));
+                      }}
+                    />
+                  </div>
 
-              <form onSubmit={handleSubmit}>
-                <div className="mb-4 flex flex-col">
-                  <label htmlFor="amount">Amount</label>
-                  <input
-                    className="focus:ring-primary-green block w-full rounded-md border-0 px-4 py-1.5 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-base"
-                    type="number"
-                    name="amount"
-                    value={String(transactionFormValues.amount)}
-                    onChange={(e) => {
-                      setTransactionFormValues((prevValues) => ({
-                        ...prevValues,
-                        amount: e.target.value,
-                      }));
-                    }}
-                  />
-                </div>
+                  <div className="mb-4">
+                    <span>Category</span>
 
-                <div className="mb-4">
-                  <span>Category</span>
+                    <div className="mb-2 flex flex-wrap gap-4">
+                      {categories?.map((category) => (
+                        <button
+                          key={category.id}
+                          type="button"
+                          onClick={() => setSelectedCategoryId(category.id)}
+                          className={classNames(
+                            'h-full border border-gray-200 px-4 py-2 font-semibold shadow-sm',
+                            selectedCategoryId === category.id && 'bg-gray-100',
+                          )}
+                        >
+                          {category.name}
+                        </button>
+                      ))}
+                    </div>
+                  </div>
 
-                  <div className="mb-2 flex flex-wrap gap-4">
-                    {categories?.map((category) => (
-                      <button
-                        key={category.id}
-                        type="button"
-                        onClick={() => setSelectedCategoryId(category.id)}
-                        className={classNames(
-                          'h-full border border-gray-200 px-4 py-2 font-semibold shadow-sm',
-                          selectedCategoryId === category.id && 'bg-gray-100',
-                        )}
-                      >
-                        {category.name}
-                      </button>
-                    ))}
+                  <div className="flex flex-col">
+                    <label htmlFor="date">Date</label>
+                    <input
+                      className="focus:ring-primary-green block w-full rounded-md border-0 px-4 py-1.5 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-base"
+                      name="date"
+                      type="date"
+                      value={transactionFormValues.date}
+                      onChange={(e) => {
+                        setTransactionFormValues((prevValues) => ({
+                          ...prevValues,
+                          date: e.target.value,
+                        }));
+                      }}
+                    />
+                  </div>
+
+                  <div className="flex flex-col">
+                    <label htmlFor="note">Note</label>
+                    <textarea
+                      onChange={(e) => {
+                        setTransactionFormValues((prevValues) => ({
+                          ...prevValues,
+                          description: e.target.value,
+                        }));
+                      }}
+                      value={transactionFormValues.description ?? ''}
+                      className="focus:ring-primary-green block w-full rounded-md border-0 px-4 py-1.5 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-base"
+                      name="description"
+                      placeholder="Add description"
+                    />
                   </div>
                 </div>
+              </div>
 
-                <div className="flex flex-col">
-                  <label htmlFor="date">Date</label>
-                  <input
-                    className="focus:ring-primary-green block w-full rounded-md border-0 px-4 py-1.5 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-base"
-                    name="date"
-                    type="date"
-                    value={transactionFormValues.date}
-                    onChange={(e) => {
-                      setTransactionFormValues((prevValues) => ({
-                        ...prevValues,
-                        date: e.target.value,
-                      }));
-                    }}
-                  />
-                </div>
-
-                <div className="mb-4 flex flex-col">
-                  <label htmlFor="note">Note</label>
-                  <textarea
-                    onChange={(e) => {
-                      setTransactionFormValues((prevValues) => ({
-                        ...prevValues,
-                        description: e.target.value,
-                      }));
-                    }}
-                    value={transactionFormValues.description ?? ''}
-                    className="focus:ring-primary-green block w-full rounded-md border-0 px-4 py-1.5 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-base"
-                    name="description"
-                    placeholder="Add description"
-                  />
-                </div>
-
+              <div className="z-10 p-4 shadow-md-top">
                 <button
-                  type="submit"
+                  onClick={handleSubmit}
                   className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   {submitButtonLabel ?? 'Submit'}
                 </button>
-              </form>
+              </div>
             </Dialog.Panel>
           </Transition.Child>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -39,6 +39,10 @@ module.exports = {
         '4xl': '20px',
         '5xl': '24px',
       },
+      boxShadow: {
+        'md-top':
+          '0 -4px 6px -1px rgb(0, 0, 0, 0.1), 0 -2px 4px -2px rgb(0, 0, 0, 0.1)',
+      },
     },
   },
   plugins: [require('@tailwindcss/forms')],
