e19fc1ea3f269e42e3892e94d8f9de70a3d46c27
hihhhello <grigori.ratushniak@gmail.com>
2024-01-10 19:09:08 -0600

    adjust colors

--- src/shared/ui/ManageCategoryModal.tsx
+++ src/shared/ui/ManageCategoryModal.tsx
@@ -126,7 +126,7 @@ export const ManageCategoryModal = ({
               <div className="z-10 border-t-2 p-4">
                 <button
                   onClick={handleSubmit}
-                  className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="block w-full rounded-md bg-main-blue px-3.5 py-2.5 text-sm text-white shadow-sm hover:bg-main-blue/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-main-blue"
                 >
                   {submitButtonLabel ?? 'Submit'}
                 </button>
