46cd77f88f7b6161731e5017a713400e69642720
hihhhello <grigori.ratushniak@gmail.com>
2024-01-12 23:46:59 -0600

    add raw tabs animation

--- src/app/_ui/HomePage/HomePageContent.tsx
+++ src/app/_ui/HomePage/HomePageContent.tsx
@@ -10,11 +10,20 @@ import {
 } from '@/shared/utils/helpers';
 import { api } from '@/shared/api/api';
 import { useBoolean } from '@/shared/utils/hooks';
-import { useState } from 'react';
+import React, {
+  ReactNode,
+  RefObject,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import { Transaction } from '@/shared/types/transactionTypes';
 import { FinancialOperationType } from '@/shared/types/globalTypes';
 import { EditTransactionModal } from '@/features/EditTransactionModal';
 import { RecurrentTransaction } from '@/shared/types/recurrentTransactionTypes';
+import { sum } from 'lodash';
+import { twMerge } from 'tailwind-merge';
 
 type HomePageContentProps = {
   transactions: Transaction[];
@@ -94,14 +103,29 @@ export const HomePageContent = ({
     },
   );
 
+  const [tab, setTab] = useState('lastPayments');
+
   return (
     <div>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
         <div className="flex max-h-[715px] flex-col rounded-3xl bg-main-paper p-4">
           <div className="mb-6">
-            <div className="inline-block rounded-full border border-main-dark px-6 py-2">
-              <span className="text-main-dark">Last payments</span>
-            </div>
+            <Tabs value={tab}>
+              <span
+                key="lastPayments"
+                onClick={() => setTab('lastPayments')}
+                className="cursor-pointer px-4 text-main-dark"
+              >
+                Last payments
+              </span>
+              <span
+                key="lastPayments2"
+                onClick={() => setTab('lastPayments2')}
+                className="cursor-pointer px-4 text-main-dark"
+              >
+                Upcoming payments
+              </span>
+            </Tabs>
           </div>
 
           <div className="flex h-full flex-col gap-4 overflow-y-auto">
@@ -200,6 +224,82 @@ export const HomePageContent = ({
         }}
         selectedTransaction={selectedTransaction}
       />
+    </div>
+  );
+};
+
+type TabsProps = {
+  children: ReactNode;
+  value: string;
+};
+
+const Tabs = (props: TabsProps) => {
+  const [indicatorWidth, setIndicatorWidth] = useState(0);
+  const [leftOffset, setLeftOffset] = useState(0);
+  const tabs = useRef<HTMLDivElement>(null);
+  const valueToIndex = useMemo(() => new Map(), []);
+  const childrenRef = useRef([]);
+
+  let childIndex = 0;
+  const children = React.Children.map(props.children, (child, index) => {
+    if (!React.isValidElement(child)) {
+      return null;
+    }
+
+    const childValue = child.key === undefined ? childIndex : child.key;
+
+    valueToIndex.set(childValue, childIndex);
+
+    // Ensure the tabRefs array is large enough
+    if (!childrenRef.current[index]) {
+      childrenRef.current[index] = React.createRef();
+    }
+
+    childIndex += 1;
+
+    const isCurrentTab = props.value === child.key;
+
+    return React.cloneElement(child, {
+      ref: childrenRef.current[index],
+      className: twMerge(
+        child.props.className,
+        isCurrentTab && 'text-white transition-colors duration-300',
+      ),
+    });
+  });
+
+  useEffect(() => {
+    if (!tabs.current || !children) {
+      return;
+    }
+
+    const activeTabIndex = valueToIndex.get(props.value);
+    const activeTabRef = childrenRef.current[activeTabIndex];
+
+    if (activeTabRef.current) {
+      const tabRect = activeTabRef.current.getBoundingClientRect();
+      const tabsRect = tabs.current.getBoundingClientRect();
+
+      const currentLeftOffset = tabRect.left - tabsRect.left;
+      setLeftOffset(currentLeftOffset);
+
+      const currentTabWidth = tabRect.width;
+      setIndicatorWidth(currentTabWidth);
+    }
+  }, [children, props.value, valueToIndex]);
+
+  return (
+    <div className="relative inline-block rounded-full border border-main-dark py-2">
+      <div className="relative z-20" ref={tabs}>
+        {children}
+      </div>
+
+      <div
+        className="absolute top-0 z-10 h-full rounded-full bg-main-dark transition-all duration-300 ease-[cubic-bezier(0.4,0,0.2,1)]"
+        style={{ width: indicatorWidth, left: leftOffset }}
+      >
+        <span className="sr-only">indicator</span>
+      </div>
     </div>
   );
 };
