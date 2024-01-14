07d3d7c1fc47a18d25a77dfcad905cd3a59e551f
hihhhello <grigori.ratushniak@gmail.com>
2024-01-14 12:12:27 -0600

    make period filter select as feature component

--- src/app/_ui/HomePage/ui/HomePageContentDesktop.tsx
+++ src/app/_ui/HomePage/ui/HomePageContentDesktop.tsx
@@ -12,10 +12,7 @@ import {
 import { FinancialOperationType } from '@/shared/types/globalTypes';
 import { EditTransactionModal } from '@/features/EditTransactionModal';
 import { RecurrentTransaction } from '@/shared/types/recurrentTransactionTypes';
-import { Listbox, Menu, Transition } from '@headlessui/react';
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
-import { upperFirst } from 'lodash';
-import { ChevronDownIcon } from '@/shared/icons/ChevronDownIcon';
+import { TransactionsPeriodFilterSelect } from '@/features/TransactionsPeriodFilterSelect';
 
 type HomePageContentDesktopProps = {
   transactions: Transaction[];
@@ -48,87 +45,10 @@ export const HomePageContentDesktop = ({
               <span className="text-main-dark">Last payments</span>
             </div>
 
-            <Listbox value={filter} onChange={handleChangeFilter}>
-              {({ open }) => (
-                <>
-                  <div className="relative mt-2">
-                    <Listbox.Button className="relative w-full min-w-[144px] cursor-pointer rounded-full border border-main-blue bg-main-blue py-2 pl-4 pr-4 text-left text-white">
-                      <span className="flex items-center">
-                        <span className="block truncate">
-                          {upperFirst(filter)}
-                        </span>
-                      </span>
-
-                      <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                        <ChevronDownIcon
-                          className={classNames(
-                            'h-5 w-5 text-white transition-transform',
-                            open && 'rotate-180',
-                          )}
-                          aria-hidden="true"
-                        />
-                      </span>
-                    </Listbox.Button>
-
-                    <Transition
-                      show={open}
-                      as={Fragment}
-                      leave="transition ease-in duration-100"
-                      leaveFrom="opacity-100"
-                      leaveTo="opacity-0"
-                    >
-                      <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                        {['all', 'today', 'month', 'year'].map((period) => (
-                          <Listbox.Option
-                            key={period}
-                            className={({ active }) =>
-                              classNames(
-                                active
-                                  ? 'bg-main-blue text-white'
-                                  : 'text-gray-900',
-                                'relative cursor-default select-none py-2 pl-3 pr-9',
-                              )
-                            }
-                            value={period}
-                          >
-                            {({ selected, active }) => (
-                              <>
-                                <div className="flex items-center">
-                                  <span
-                                    className={classNames(
-                                      selected
-                                        ? 'font-semibold'
-                                        : 'font-normal',
-                                      'ml-3 block truncate',
-                                    )}
-                                  >
-                                    {upperFirst(period)}
-                                  </span>
-                                </div>
-
-                                {selected ? (
-                                  <span
-                                    className={classNames(
-                                      active ? 'text-white' : 'text-main-blue',
-                                      'absolute inset-y-0 right-0 flex items-center pr-4',
-                                    )}
-                                  >
-                                    <CheckIcon
-                                      className="h-5 w-5"
-                                      aria-hidden="true"
-                                    />
-                                  </span>
-                                ) : null}
-                              </>
-                            )}
-                          </Listbox.Option>
-                        ))}
-                      </Listbox.Options>
-                    </Transition>
-                  </div>
-                </>
-              )}
-            </Listbox>
+            <TransactionsPeriodFilterSelect
+              filter={filter}
+              handleChangeFilter={handleChangeFilter}
+            />
           </div>
 
           <div className="flex h-full flex-col gap-4 overflow-y-auto">
---
+++ src/features/TransactionsPeriodFilterSelect.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+import { Fragment } from 'react';
+
+import { classNames } from '@/shared/utils/helpers';
+import { TransactionPeriodFilter } from '@/shared/types/transactionTypes';
+import { Listbox, Transition } from '@headlessui/react';
+import { CheckIcon } from '@heroicons/react/20/solid';
+import { upperFirst } from 'lodash';
+import { ChevronDownIcon } from '@/shared/icons/ChevronDownIcon';
+
+type TransactionsPeriodFilterSelectProps = {
+  filter: TransactionPeriodFilter;
+  handleChangeFilter: (newFilter: TransactionPeriodFilter) => void;
+};
+
+export const TransactionsPeriodFilterSelect = ({
+  filter,
+  handleChangeFilter,
+}: TransactionsPeriodFilterSelectProps) => {
+  return (
+    <Listbox value={filter} onChange={handleChangeFilter}>
+      {({ open }) => (
+        <>
+          <div className="relative mt-2">
+            <Listbox.Button className="relative w-full min-w-[144px] cursor-pointer rounded-full border border-main-blue bg-main-blue py-2 pl-4 pr-4 text-left text-white">
+              <span className="flex items-center">
+                <span className="block truncate">{upperFirst(filter)}</span>
+              </span>
+
+              <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                <ChevronDownIcon
+                  className={classNames(
+                    'h-5 w-5 text-white transition-transform',
+                    open && 'rotate-180',
+                  )}
+                  aria-hidden="true"
+                />
+              </span>
+            </Listbox.Button>
+
+            <Transition
+              show={open}
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {['all', 'today', 'month', 'year'].map((period) => (
+                  <Listbox.Option
+                    key={period}
+                    className={({ active }) =>
+                      classNames(
+                        active ? 'bg-main-blue text-white' : 'text-gray-900',
+                        'relative cursor-default select-none py-2 pl-3 pr-9',
+                      )
+                    }
+                    value={period}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <div className="flex items-center">
+                          <span
+                            className={classNames(
+                              selected ? 'font-semibold' : 'font-normal',
+                              'ml-3 block truncate',
+                            )}
+                          >
+                            {upperFirst(period)}
+                          </span>
+                        </div>
+
+                        {selected ? (
+                          <span
+                            className={classNames(
+                              active ? 'text-white' : 'text-main-blue',
+                              'absolute inset-y-0 right-0 flex items-center pr-4',
+                            )}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </>
+      )}
+    </Listbox>
+  );
+};
