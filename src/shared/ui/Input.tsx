3c8106764622b6e2533bbaa28977275fce792725
hihhhello <grigori.ratushniak@gmail.com>
2024-01-17 14:01:43 -0600

    forward input ref

--- src/shared/ui/Input.tsx
+++ src/shared/ui/Input.tsx
@@ -1,15 +1,19 @@
+import { Ref, forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-export const Input = ({
-  className,
-  ...props
-}: JSX.IntrinsicElements['input']) => (
-  <input
-    placeholder="Enter here"
-    className={twMerge(
-      'block w-full rounded-2xl border-0 px-4 py-3 text-main-dark ring-1 ring-inset ring-transparent placeholder:text-gray-400 focus:ring-main-blue',
-      className,
-    )}
-    {...props}
-  />
-);
+export const Input = forwardRef(function Input(
+  { className, ...props }: JSX.IntrinsicElements['input'],
+  ref: Ref<HTMLInputElement>,
+) {
+  return (
+    <input
+      ref={ref}
+      placeholder="Enter here"
+      className={twMerge(
+        'block w-full rounded-2xl border-0 px-4 py-3 text-main-dark ring-1 ring-inset ring-transparent placeholder:text-gray-400 focus:ring-main-blue',
+        className,
+      )}
+      {...props}
+    />
+  );
+});
