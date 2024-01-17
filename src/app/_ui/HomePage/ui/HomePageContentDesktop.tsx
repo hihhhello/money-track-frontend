df092ee85795f108e7db528bc9653753f2f22836
hihhhello <grigori.ratushniak@gmail.com>
2024-01-16 21:28:03 -0600

    add quick actions for desktop

--- src/app/_ui/HomePage/ui/HomePageContentDesktop.tsx
+++ src/app/_ui/HomePage/ui/HomePageContentDesktop.tsx
@@ -4,7 +4,7 @@ import { Fragment, useState } from 'react';
 import { format, parseISO } from 'date-fns';
 
 import { classNames, formatToUSDCurrency } from '@/shared/utils/helpers';
-import { useBoolean } from '@/shared/utils/hooks';
+import { useBoolean, useLoadingToast } from '@/shared/utils/hooks';
 import {
   Transaction,
   TransactionPeriodFilter,
@@ -13,6 +13,12 @@ import { FinancialOperationType } from '@/shared/types/globalTypes';
 import { EditTransactionModal } from '@/features/EditTransactionModal';
 import { RecurrentTransaction } from '@/shared/types/recurrentTransactionTypes';
 import { TransactionsPeriodFilterSelect } from '@/features/TransactionsPeriodFilterSelect';
+import { Menu, Transition } from '@headlessui/react';
+import { ThreeDotsVerticalIcon } from '@/shared/icons/ThreeDotsVerticalIcon';
+import { PencilIcon } from '@heroicons/react/24/solid';
+import { TrashIcon } from '@heroicons/react/24/outline';
+import { api } from '@/shared/api/api';
+import { useQueryClient } from '@tanstack/react-query';
 
 type HomePageContentDesktopProps = {
   transactions: Transaction[];
@@ -27,6 +33,9 @@ export const HomePageContentDesktop = ({
   filter,
   handleChangeFilter,
 }: HomePageContentDesktopProps) => {
+  const queryClient = useQueryClient();
+  const loadingToast = useLoadingToast();
+
   const {
     value: isEditTransactionModalOpen,
     setTrue: handleOpenEditTransactionModal,
@@ -35,6 +44,29 @@ export const HomePageContentDesktop = ({
 
   const [selectedTransaction, setSelectedTransaction] =
     useState<Transaction | null>(null);
+
+  const handleDeleteTransaction = (transactionId: number) => {
+    const toastId = loadingToast.showLoading('Deleting your transaction...');
+
+    return api.transactions
+      .deleteOne({
+        params: {
+          transactionId,
+        },
+      })
+      .then(() => {
+        loadingToast.handleSuccess({
+          toastId,
+          message: 'You successfully deleted transaction.',
+        });
+        queryClient.refetchQueries({
+          queryKey: ['api.transactions.getAll'],
+        });
+      })
+      .catch(() => {
+        loadingToast.handleError({ toastId, message: 'Error' });
+      });
+  };
 
   return (
     <div>
@@ -51,15 +83,11 @@ export const HomePageContentDesktop = ({
             />
           </div>
 
-          <div className="flex h-full flex-col gap-4 overflow-y-auto">
+          <div className="flex h-full flex-col gap-4 overflow-y-auto overflow-x-visible">
             {transactions.map((transaction) => (
               <button
-                onClick={() => {
-                  setSelectedTransaction(transaction);
-                  handleOpenEditTransactionModal();
-                }}
                 key={transaction.id}
-                className="flex flex-col rounded-lg bg-white px-4 py-1 pr-2 hover:bg-gray-200 sm:flex-row sm:items-center sm:justify-between"
+                className="flex flex-col rounded-lg bg-white px-4 py-1 pr-2 sm:flex-row sm:items-center sm:justify-between"
               >
                 <div className="flex w-full flex-grow flex-col items-start">
                   <span className="w-full break-words text-left">
@@ -87,6 +115,94 @@ export const HomePageContentDesktop = ({
                     {formatToUSDCurrency(parseFloat(transaction.amount))}
                   </p>
                 </div>
+
+                <Menu as="div" className="relative ml-3">
+                  {({ open }) => (
+                    <>
+                      <Menu.Button
+                        className={classNames(
+                          'rounded-md bg-main-blue',
+                          open && 'bg-main-dark',
+                        )}
+                      >
+                        <ThreeDotsVerticalIcon className="text-white" />
+                      </Menu.Button>
+
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-200"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute left-0 top-0 z-10 w-[115px] origin-top-left -translate-x-full rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <button
+                                onClick={() => {
+                                  setSelectedTransaction(transaction);
+                                  handleOpenEditTransactionModal();
+                                }}
+                                className={classNames(
+                                  'flex w-full items-center gap-2 rounded-t-md px-4 py-2',
+                                  active && 'bg-main-blue/10',
+                                )}
+                              >
+                                <PencilIcon
+                                  className={classNames(
+                                    'h-5 w-5 text-gray-500',
+                                    active && 'text-main-blue',
+                                  )}
+                                />
+
+                                <span
+                                  className={classNames(
+                                    'h-5 w-5 text-gray-500',
+                                    active && 'text-main-blue',
+                                  )}
+                                >
+                                  Edit
+                                </span>
+                              </button>
+                            )}
+                          </Menu.Item>
+
+                          <Menu.Item>
+                            {({ active }) => (
+                              <button
+                                onClick={() =>
+                                  handleDeleteTransaction(transaction.id)
+                                }
+                                className={classNames(
+                                  'flex w-full items-center gap-2 rounded-b-md px-4 py-2',
+                                  active && 'bg-red-100',
+                                )}
+                              >
+                                <TrashIcon
+                                  className={classNames(
+                                    'h-5 w-5 text-gray-500',
+                                    active && 'text-red-600',
+                                  )}
+                                />
+
+                                <span
+                                  className={classNames(
+                                    'h-5 w-5 text-gray-500',
+                                    active && 'text-red-600',
+                                  )}
+                                >
+                                  Delete
+                                </span>
+                              </button>
+                            )}
+                          </Menu.Item>
+                        </Menu.Items>
+                      </Transition>
+                    </>
+                  )}
+                </Menu>
               </button>
             ))}
           </div>
