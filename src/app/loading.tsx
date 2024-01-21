711f7fdc05d64ce3bcd5a6db9fece54e8baba69f
hihhhello <grigori.ratushniak@gmail.com>
2024-01-20 19:00:09 -0600

    fix layout for auth pages and loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from '@/shared/ui/Spinner';
 
 const LoadingPage = () => {
   return (
-    <div className="grid h-full place-items-center">
+    <div className="grid h-full flex-grow place-items-center">
       <Spinner className="fill-main-blue" width="64" height="64" />
     </div>
   );
