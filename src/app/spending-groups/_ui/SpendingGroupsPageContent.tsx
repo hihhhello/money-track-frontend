604209764d7e89637203d60d6470fbfb622aa04b
hihhhello <grigori.ratushniak@gmail.com>
2024-01-26 15:18:20 -0600

    add queryKey

--- src/app/spending-groups/_ui/SpendingGroupsPageContent.tsx
+++ src/app/spending-groups/_ui/SpendingGroupsPageContent.tsx
@@ -22,6 +22,7 @@ export const SpendingGroupsPageContent = ({
   const { data: spendingGroups, refetch: refetchSpendingGroups } = useQuery({
     queryFn: api.spendingGroups.getAll,
     initialData: initialSpendingGroups,
+    queryKey: ['api.spendingGroups.getAll'],
   });
 
   const {
