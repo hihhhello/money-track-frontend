6bd35e0cd5227d8199fb0bfbe6c2072aa8af7eda
hihhhello <grigori.ratushniak@gmail.com>
2024-02-09 08:19:18 -0600

    make recurrent transactions table view

--- src/app/recurrent-transactions/_ui/RecurrentTransactionsPageContent.tsx
+++ src/app/recurrent-transactions/_ui/RecurrentTransactionsPageContent.tsx
@@ -8,11 +8,15 @@ import {
   FinancialOperationTypeValue,
 } from '@/shared/types/globalTypes';
 import { RecurrentTransaction } from '@/shared/types/recurrentTransactionTypes';
-import { formatUSDDecimal } from 'hihhhello-utils';
+import { classNames, formatUSDDecimal } from 'hihhhello-utils';
 import { useBoolean } from 'hihhhello-utils';
 import { useQuery } from '@tanstack/react-query';
 import { format, parseISO } from 'date-fns';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import { ThreeDotsVerticalIcon } from '@/shared/icons/ThreeDotsVerticalIcon';
+import { TrashIcon } from '@/shared/icons/TrashIcon';
+import { PencilIcon } from '@heroicons/react/24/solid';
 
 type RecurrentTransactionsPageContentProps = {
   recurrentTransactions: RecurrentTransaction[];
@@ -70,69 +74,191 @@ export const RecurrentTransactionsPageContent = ({
           </button>
         </div>
 
-        <div className="grid h-full grid-cols-1 gap-4 overflow-y-auto p-2 sm:grid-cols-4">
-          {recurrentTransactions?.map((transaction) => (
-            <div
-              key={transaction.id}
-              className="rounded-3xl bg-main-paper shadow-md"
-            >
-              <div className="rounded-t-3xl bg-main-blue px-4 py-2 text-white">
-                <h3>{transaction.category.name}</h3>
-              </div>
-
-              <div className="p-4">
-                <p className="text-gray-700">Type: {transaction.type}</p>
-
-                <p className="text-gray-700">
-                  Frequency: {transaction.frequency}
-                </p>
-
-                <p className="text-gray-700">
-                  Next Transaction:{' '}
-                  {format(
-                    parseISO(transaction.next_transaction),
-                    'EEEE, dd MMMM',
-                  )}
-                </p>
-
-                <p className="text-gray-700">
-                  Amount: {formatUSDDecimal(parseFloat(transaction.amount))}
-                </p>
-
-                {transaction.description && (
-                  <p className="text-gray-700">
-                    Description: {transaction.description}
-                  </p>
-                )}
-
-                {transaction.start_date && (
-                  <p className="text-gray-700">
-                    Start Date:{' '}
-                    {format(parseISO(transaction.start_date), 'EEEE, dd MMMM')}
-                  </p>
-                )}
-
-                {transaction.end_date && (
-                  <p className="text-gray-700">
-                    End Date:{' '}
-                    {format(parseISO(transaction.end_date), 'EEEE, dd MMMM')}
-                  </p>
-                )}
-              </div>
-
-              <div className="w-full border-t-2 px-4 py-2">
-                <button
-                  onClick={() => {
-                    setSelectedRecurrentTransaction(transaction);
-                    handleOpenEditTransactionModal();
-                  }}
-                  className="text-main-blue hover:text-main-blue/70"
+        <div className="relative overflow-auto rounded-3xl">
+          <table className="relative min-w-full  rounded-3xl bg-main-paper">
+            <thead className="sticky top-0 z-10 bg-main-paper">
+              <tr>
+                <th
+                  scope="col"
+                  className="text-text-dark py-3.5 pl-6 pr-3 text-left text-sm font-semibold"
                 >
-                  Edit
-                </button>
-              </div>
-            </div>
-          ))}
+                  Type
+                </th>
+
+                <th
+                  scope="col"
+                  className="focus-primary-green text-text-dark cursor-pointer px-3 py-3.5 text-left text-sm font-semibold"
+                  tabIndex={0}
+                >
+                  Amount
+                </th>
+
+                <th
+                  scope="col"
+                  className="focus-primary-green text-text-dark cursor-pointer px-3 py-3.5 text-left text-sm font-semibold"
+                  tabIndex={0}
+                >
+                  Category
+                </th>
+
+                <th
+                  scope="col"
+                  className="focus-primary-green text-text-dark cursor-pointer px-3 py-3.5 text-left text-sm font-semibold"
+                  tabIndex={0}
+                >
+                  Description
+                </th>
+
+                <th
+                  scope="col"
+                  className="text-text-dark px-3 py-3.5 text-left text-sm font-semibold"
+                >
+                  Next Transaction
+                </th>
+
+                <th
+                  scope="col"
+                  className="text-text-dark px-3 py-3.5 text-left text-sm font-semibold"
+                >
+                  Start Date
+                </th>
+
+                <th
+                  scope="col"
+                  className="text-text-dark px-3 py-3.5 text-left text-sm font-semibold"
+                >
+                  End Date
+                </th>
+
+                <th
+                  scope="col"
+                  className="text-text-dark px-3 py-3.5 text-left text-sm font-semibold"
+                >
+                  Frequency
+                </th>
+
+                <th scope="col" className="py-3.5 pl-3 pr-4">
+                  <span className="sr-only">Actions</span>
+                </th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {recurrentTransactions?.map((transaction) => {
+                const isTransactionSelected =
+                  selectedRecurrentTransaction?.id === transaction.id;
+
+                return (
+                  <tr
+                    key={transaction.id}
+                    className={classNames(
+                      '',
+                      isTransactionSelected && 'bg-gray-50',
+                    )}
+                  >
+                    <td className="text-text-regular whitespace-nowrap py-2 pl-6 pr-3 text-sm">
+                      {transaction.type}
+                    </td>
+
+                    <td className="text-text-regular whitespace-nowrap px-3 py-2 text-sm">
+                      {formatUSDDecimal(parseFloat(transaction.amount))}
+                    </td>
+
+                    <td className="text-text-regular whitespace-nowrap px-3 py-2 text-sm">
+                      {transaction.category.name}
+                    </td>
+
+                    <td className="text-text-regular whitespace-nowrap px-3 py-2 text-sm">
+                      {transaction.description}
+                    </td>
+
+                    <td className="text-text-regular whitespace-nowrap px-3 py-2 text-sm">
+                      {format(
+                        parseISO(transaction.next_transaction),
+                        'EEE, dd MMM',
+                      )}
+                    </td>
+
+                    <td className="text-text-regular whitespace-nowrap px-3 py-2 text-sm">
+                      {format(parseISO(transaction.start_date), 'dd MMM yyyy')}
+                    </td>
+
+                    <td className="text-text-regular whitespace-nowrap px-3 py-2 text-sm">
+                      {transaction.end_date
+                        ? format(parseISO(transaction.end_date), 'dd MMM yyyy')
+                        : 'None'}
+                    </td>
+
+                    <td className="text-text-regular whitespace-nowrap px-3 py-2 text-sm">
+                      {transaction.frequency}
+                    </td>
+
+                    <td className="text-text-regular whitespace-nowrap rounded-r-md px-3 py-2 pr-4 text-sm">
+                      <Menu as="div" className="relative ml-3">
+                        {({ open }) => (
+                          <>
+                            <Menu.Button
+                              className={classNames(
+                                'rounded-md bg-main-blue',
+                                open && 'bg-main-dark',
+                              )}
+                            >
+                              <ThreeDotsVerticalIcon className="text-white" />
+                            </Menu.Button>
+
+                            <Transition
+                              as={Fragment}
+                              enter="transition ease-out duration-200"
+                              enterFrom="transform opacity-0 scale-95"
+                              enterTo="transform opacity-100 scale-100"
+                              leave="transition ease-in duration-75"
+                              leaveFrom="transform opacity-100 scale-100"
+                              leaveTo="transform opacity-0 scale-95"
+                            >
+                              <Menu.Items className="absolute left-0 top-0 z-10 w-[115px] origin-top-left -translate-x-full rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                <Menu.Item>
+                                  {({ active }) => (
+                                    <button
+                                      onClick={() => {
+                                        setSelectedRecurrentTransaction(
+                                          transaction,
+                                        );
+                                        handleOpenEditTransactionModal();
+                                      }}
+                                      className={classNames(
+                                        'flex w-full items-center gap-2 rounded-t-md px-4 py-2',
+                                        active && 'bg-main-blue/10',
+                                      )}
+                                    >
+                                      <PencilIcon
+                                        className={classNames(
+                                          'h-5 w-5 text-gray-500',
+                                          active && 'text-main-blue',
+                                        )}
+                                      />
+
+                                      <span
+                                        className={classNames(
+                                          'h-5 w-5 text-gray-500',
+                                          active && 'text-main-blue',
+                                        )}
+                                      >
+                                        Edit
+                                      </span>
+                                    </button>
+                                  )}
+                                </Menu.Item>
+                              </Menu.Items>
+                            </Transition>
+                          </>
+                        )}
+                      </Menu>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
       </div>
 
@@ -143,7 +269,10 @@ export const RecurrentTransactionsPageContent = ({
       />
 
       <EditRecurrentTransactionModal
-        handleClose={handleCloseEditTransactionModal}
+        handleClose={() => {
+          handleCloseEditTransactionModal();
+          setSelectedRecurrentTransaction(null);
+        }}
         isModalOpen={isEditTransactionModalOpen}
         selectedRecurrentTransaction={selectedRecurrentTransaction}
       />
