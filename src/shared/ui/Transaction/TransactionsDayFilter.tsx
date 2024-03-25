27f0281cca3b4f235d7d5b14dc35b18215712ca9
hihhhello <grigori.ratushniak@gmail.com>
2024-03-24 20:13:46 -0500

    add div wrapper to fix filter filling flex container

--- src/shared/ui/Transaction/TransactionsDayFilter.tsx
+++ src/shared/ui/Transaction/TransactionsDayFilter.tsx
@@ -14,26 +14,28 @@ export const TransactionsDayFilter = ({
   value,
 }: TransactionsDayFilterProps) => {
   return (
-    <div className="relative flex min-w-[110px] items-center justify-between rounded-full border border-main-blue bg-main-blue px-2 py-1.5 text-left text-white sm:min-w-[144px]">
-      <button onClick={() => handleChange(subDays(value, 1))}>
-        <ChevronLeftIcon
-          className={classNames('h-5 w-5 text-white')}
-          aria-hidden="true"
-        />
-      </button>
+    <div>
+      <div className="relative flex min-w-[110px] items-center justify-between rounded-full border border-main-blue bg-main-blue px-2 py-1.5 text-left text-white sm:min-w-[144px]">
+        <button onClick={() => handleChange(subDays(value, 1))}>
+          <ChevronLeftIcon
+            className={classNames('h-5 w-5 text-white')}
+            aria-hidden="true"
+          />
+        </button>
 
-      <span className="flex items-center">
-        <span className="block truncate text-sm sm:text-base">
-          {format(value, 'dd MMM yyyy')}
+        <span className="flex items-center">
+          <span className="block truncate text-sm sm:text-base">
+            {format(value, 'dd MMM yyyy')}
+          </span>
         </span>
-      </span>
 
-      <button onClick={() => handleChange(addDays(value, 1))}>
-        <ChevronRightIcon
-          className={classNames('h-5 w-5 text-white')}
-          aria-hidden="true"
-        />
-      </button>
+        <button onClick={() => handleChange(addDays(value, 1))}>
+          <ChevronRightIcon
+            className={classNames('h-5 w-5 text-white')}
+            aria-hidden="true"
+          />
+        </button>
+      </div>
     </div>
   );
 };
