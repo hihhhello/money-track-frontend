9027016dbc86642245891954b9f988c7aeb56a4a
hihhhello <grigori.ratushniak@gmail.com>
2024-06-26 08:42:07 -0500

    add input sizes

--- src/shared/ui/Input.tsx
+++ src/shared/ui/Input.tsx
@@ -1,8 +1,27 @@
 import { Ref, forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
+const InputSize = {
+  Small: 'sm',
+  Medium: 'md',
+  Large: 'lg',
+} as const;
+
+type InputSizeType = (typeof InputSize)[keyof typeof InputSize];
+
+type InputProps = { size?: InputSizeType } & Omit<
+  JSX.IntrinsicElements['input'],
+  'size'
+>;
+
+const inputSizeClasses: Record<InputSizeType, string> = {
+  [InputSize.Small]: 'text-sm px-2 py-1.5',
+  [InputSize.Medium]: 'text-base px-4 py-3',
+  [InputSize.Large]: 'text-lg px-5 py-4',
+};
+
 export const Input = forwardRef(function Input(
-  { className, ...props }: JSX.IntrinsicElements['input'],
+  { className, size = InputSize.Medium, ...props }: InputProps,
   ref: Ref<HTMLInputElement>,
 ) {
   return (
@@ -10,7 +29,8 @@ export const Input = forwardRef(function Input(
       ref={ref}
       placeholder="Enter here"
       className={twMerge(
-        'block w-full rounded-2xl border-0 px-4 py-3 text-main-dark ring-1 ring-inset ring-transparent placeholder:text-gray-400 focus:ring-main-blue',
+        'block w-full rounded-2xl border-0 text-main-dark ring-1 ring-inset ring-transparent placeholder:text-gray-400 focus:ring-main-blue',
+        inputSizeClasses[size],
         className,
       )}
       {...props}
